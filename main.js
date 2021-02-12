326521a4349fd964547311292b8cae6a7a46a870
Paolo Cargnin <paolocargnin@boom.ink>
2021-02-12 10:12:07 +0100

    Count the past

--- main.js
+++ main.js
@@ -14,21 +14,32 @@ Vue.createApp({
         }
     },
 
+
     methods: {
         decideComputerMove() {
             console.log(this.userMoves)
-
             // get the last 20 moves of the user
             // check the amount of each choice 
 
             //console.log( amount of rock moves)
             //console.log( amount of scissor moves)
             //console.log( amount of paper moves)
+            let start = this.userMoves.length - 20
+            if ( start < 0){
+                start = 0
+            }
             
+            let movesAmount = [null, 0,0,0]
+            for (let i = start; i < this.userMoves.length; i++) {
+                movesAmount[this.userMoves[i]] ++
+            }
+            console.log(movesAmount[1], "Rocks")
+            console.log(movesAmount[2], "Scissors")
+            console.log(movesAmount[3], "Papers")
+
 
             this.computerMove = parseInt(Math.random() * 3) + 1
         },
-
         gameCheck(move){
             this.userMoves.push(move)
             this.move = move
